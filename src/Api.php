cb0487cb92f81b982bbd48dbfc4988a8f5c4273c
Arne De Belser <debelserarne@gmail.com>
2023-08-04 11:04:23 +0200

    Add notice whenever credentials are incorrect or missing

--- src/Api.php
+++ src/Api.php
@@ -2,6 +2,7 @@
 
 namespace ADB\ImmoSyncWhise;
 
+use Whise\Api\Exception\AuthException;
 use Whise\Api\WhiseApi;
 
 class Api
@@ -12,8 +13,21 @@ class Api
     {
         $this->connection = $connection;
 
-        $accessToken = $this->connection->requestAccessToken($whiseUser, $whisePassword);
+        try {
+            $accessToken = $this->connection->requestAccessToken($whiseUser, $whisePassword);
 
-        $this->connection->setAccessToken($accessToken);
+            $this->connection->setAccessToken($accessToken);
+        } catch (AuthException $e) {
+            add_action('admin_notices', [$this, 'unauthorizedMessage']);
+        }
+    }
+
+    public function unauthorizedMessage()
+    {
+?>
+        <div class="notice notice-error">
+            <p><?php _e('<h3>Immo Sync Whise: Unauthorized Access</h3><span>To enable proper functionality of the Whise integration, please ensure you provide the correct login credentials in the .env file.</span>', 'immo-sync-whise'); ?></p>
+        </div>
+<?php
     }
 }
