3ed846a56d110bf282ae5879f4b3e2a4fc3afa59
Arne De Belser <debelserarne@gmail.com>
2023-08-04 10:27:04 +0200

    Create test bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Adjust the path to autoload.php if needed
+
+define('ISW_VERSION', fileatime(__DIR__));
+define('ISW_PATH', __DIR__);
+define('PLUGIN__FILE__', __FILE__);
