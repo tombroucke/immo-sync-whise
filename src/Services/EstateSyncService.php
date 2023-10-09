c4ff065d1c54af985415b92a2582f22ef301572f
ArneDeBelser <debelserarne@gmail.com>
2023-10-09 20:14:52 +0200

    Update EstateSyncService.php

--- src/Services/EstateSyncService.php
+++ src/Services/EstateSyncService.php
@@ -22,26 +22,22 @@ class EstateSyncService
         \WP_CLI::log("Fetching all estates from Whise API");
         $this->logger->info("Fetching all estates from Whise API");
 
-        $estates = $this->estateAdapter->list([
-            'LanguageId' => $_ENV['LANG'],
-        ]);
+        $estates = $this->estateAdapter->list(['LanguageId' => $_ENV['LANG']]);
 
         foreach ($estates as $estate) {
             // Save the Post
             $postId = $this->estate->save($estate);
 
             // Configure the parser
-            $this->estateParser->setMethod('add_post_meta');
-            $this->estateParser->setPostId($postId);
-            $this->estateParser->setObject($estate);
+            $this->estateParser->setMethod('add_post_meta')->setPostId($postId)->setObject($estate);
 
             // Parse the response object
             $this->estateParser->parseProperties();
             $this->estateParser->parseDetails();
             $this->estateParser->parsePictures();
 
-            \WP_CLI::success("Fetched estate, created post {$postId}");
-            $this->logger->info("Fetched estate, created post {$postId}");
+            \WP_CLI::success("Fetched estate, created post with ID #{$postId}");
+            $this->logger->info("Fetched estate, created post with ID #{$postId}");
         }
 
         \WP_CLI::success('Fetching successful');
