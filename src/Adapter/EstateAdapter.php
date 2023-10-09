95b2499119a69131549d42c868543f8d93c05f81
ArneDeBelser <debelserarne@gmail.com>
2023-10-09 11:37:57 +0200

    Refactor comments to new PHP syntax
    Refactor comments to new PHP syntax

--- src/Adapter/EstateAdapter.php
+++ src/Adapter/EstateAdapter.php
@@ -3,43 +3,23 @@
 namespace ADB\ImmoSyncWhise\Adapter;
 
 use ADB\ImmoSyncWhise\Api;
+use Whise\Api\Response\CollectionResponse;
 
 class EstateAdapter
 {
     public $api;
 
-    /**
-     * EstateAdapter constructor.
-     *
-     * @param Api $api The API instance to be used.
-     */
     public function __construct(Api $api)
     {
         $this->api = $api->connection;
     }
 
-    /**
-     * Get a list of estates based on the provided filters, sorting, and fields.
-     *
-     * @param array $filter An array of filters to apply to the query.
-     * @param array $sorting An array specifying the sorting criteria.
-     * @param array $fields An array specifying the fields to include in the result.
-     * @return array The list of estates matching the criteria.
-     */
-    public function list($filter = [], $sorting = [], $fields = [])
+    public function list(array $filter = [], array $sorting = [], array $fields = []): CollectionResponse
     {
         return $this->api->estates()->list($filter, $sorting, $fields);
     }
 
-    /**
-     * Get the details of an estate by its ID.
-     *
-     * @param int $id The ID of the estate to retrieve.
-     * @param array $filter An array of filters to apply to the query.
-     * @param array $fields An array specifying the fields to include in the result.
-     * @return array|null The details of the estate or null if not found.
-     */
-    public function get($id, $filter = [], $fields = [])
+    public function get(int $id, array $filter = [], array $fields = []): array|null
     {
         return $this->api->estates()->get($id, $filter, $fields);
     }
