e292c117496bb1fe469afa994d26ce0760b44af6
ArneDeBelser <debelserarne@gmail.com>
2023-10-11 10:43:57 +0200

    Update plugin description

--- immo-sync-whise.php
+++ immo-sync-whise.php
@@ -3,12 +3,12 @@
 /*
  * Plugin Name:       Immo Sync Whise
  * Plugin URI:        
- * Description:       Integrates the Whise API with WordPress
+ * Description:       Elevate your real estate WordPress site with the power of Whise through our Immo Sync plugin. This seamless integration simplifies the connection between your website and the Whise API, allowing you to effortlessly incorporate real-time property data and listings, enhancing your visitors' property search experience.
  * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.3
  * Author:            De Belser Arne
- * Author URI:        
+ * Author URI:        https://www.arnedebelser.be/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       immo-sync-whise
